d99f421de3ae22cf7c59f147a498abd4b6e05b8d
vegaz <66195716+tadeovegaz@users.noreply.github.com>
2023-07-25 14:06:26 -0600

    Nabvar working
    New navbar with react router dom

--- mywebsite/src/App.js
+++ mywebsite/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 import Navbar from './components/navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './pages/about';
+import Home from './pages/home';
+
 
 function App() {
+
+
   return (
-    <Navbar/>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- mywebsite/src/components/navbar.jsx
+++ mywebsite/src/components/navbar.jsx
@@ -1,8 +1,21 @@
+import { Link } from "react-router-dom";
+import "../css/navbar.css"
+
+
 const Navbar = () => {
     return (
-        <header className="bg-white">
-           
-        </header>
+        <nav className="navbar">
+            <h3 className="logo">Logo</h3>
+            <ul className="nav-links">
+                <Link to='/' className='home'>
+                    <li>Home</li>
+                </Link>
+                <Link to='/about' className='home'>
+                    <li>About</li>
+                </Link>
+                
+            </ul>
+        </nav>
     )
 }
 
